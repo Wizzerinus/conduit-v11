fc6b531f087de4eeb2e7f40deb1b1361adcce043
MultiDragon <multidragon@yandex.ru>
2022-11-05 20:03:37 +0300

    implement backups and conduit imports

--- pyconduit/setup.py
+++ pyconduit/setup.py
@@ -1,6 +1,8 @@
+import os
 import secrets
 import subprocess
 import sys
+import time
 
 import click
 import yaml
@@ -48,6 +50,52 @@ def create_admin():
         admin=True, conduit_edit=True, sheets_edit=True, conduit_generation=False, formula_edit=True
     )
     register(RegisterUser(login=username, password=password, name="Administrator", privileges=privileges))
+
+
+@cli.command()
+def backup_database():
+    backup_folder = "backups/"
+    os.makedirs(backup_folder, exist_ok=True)
+    current_backups = os.listdir(backup_folder)
+    if len(current_backups) > 5:
+        current_backups.sort()
+        os.remove(backup_folder + current_backups[0])
+
+    new_path = backup_folder + "backup_" + str(int(time.time()))
+    subprocess.run(["cp", "-r", "json-db", new_path])
+
+
+@cli.command()
+@click.argument("sheet_id")
+@click.argument("user_order_file")
+@click.argument("conduit_tsv")
+def import_conduits(sheet_id: str, user_order_file: str, conduit_tsv: str):
+    from pyconduit.shared.datastore import datastore_manager
+
+    accounts = datastore_manager.get("accounts")
+    with open(user_order_file) as f:
+        user_order = [line.strip() for line in f]
+
+    username_dict = {x.name: x.login for x in accounts.accounts.values()}
+    user_order = [username_dict[x] for x in user_order]
+
+    with open(conduit_tsv) as f:
+        conduit_data = [line.strip().split("\t") for line in f]
+
+    with datastore_manager.get("sheets").operation() as ds:
+        sheets = ds.sheets
+        if sheet_id not in sheets:
+            click.echo(f"Sheet {sheet_id} not found")
+            return
+
+        sheet = sheets[sheet_id]
+        if not sheet.conduit:
+            click.echo(f"Sheet {sheet_id} has no conduit")
+            return
+
+        conduit = sheet.conduit
+        for user_login, conduit_data in zip(user_order, conduit_data):
+            conduit.content[user_login] = conduit_data + [""] * (len(conduit.problem_names) - len(conduit_data))
 
 
 if __name__ == "__main__":
